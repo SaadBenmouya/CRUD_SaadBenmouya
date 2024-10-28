dfa1f23a5ab06d17f8784ffc1a3b190afae036c5
Saad-000 <saad.benmouya@gmail.com>
2024-10-28 09:30:29 +0100

    Initialisation du projet

---
+++ CRUD_SaadBenmouya/src/main/java/org/example/crud_saadbenmouya/CrudSaadBenmouyaApplication.java
@@ -0,0 +1,13 @@
+package org.example.crud_saadbenmouya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSaadBenmouyaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudSaadBenmouyaApplication.class, args);
+    }
+
+}
---
+++ CRUD_SaadBenmouya/src/test/java/org/example/crud_saadbenmouya/CrudSaadBenmouyaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.crud_saadbenmouya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSaadBenmouyaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
